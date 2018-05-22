74432d41e4777f9164cb8c4485076e107cb5fc83
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-05-22 23:12:13 +0530

    DONE

--- src/main/java/dbs/unicorn/teamone/dbsintuit/app/repo/EmployeeRepo.java
+++ src/main/java/dbs/unicorn/teamone/dbsintuit/app/repo/EmployeeRepo.java
@@ -1,5 +1,11 @@
 package dbs.unicorn.teamone.dbsintuit.app.repo;
 
-public interface EmployeeRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import dbs.unicorn.teamone.dbsintuit.app.entity.EmployeeEntity;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<EmployeeEntity, Long> {
 
 }
--- src/main/java/dbs/unicorn/teamone/dbsintuit/app/repo/EmployerRepo.java
+++ src/main/java/dbs/unicorn/teamone/dbsintuit/app/repo/EmployerRepo.java
@@ -1,5 +1,11 @@
 package dbs.unicorn.teamone.dbsintuit.app.repo;
 
-public interface EmployerRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import dbs.unicorn.teamone.dbsintuit.app.entity.EmployerEntity;
+
+@Repository
+public interface EmployerRepo extends JpaRepository<EmployerEntity, Long> {
 
 }
